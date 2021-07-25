5b72e6e27dba467f6dbd4d222cab3d07fb194f42
chendingtao <chendingtao@fuzhi.ai>
2021-07-26 00:46:31 +0800

    feat(vue3-base): 创建vue3-vite 项目 实验vue基础语法

--- vite_vue3_base/src/router/index.ts
+++ vite_vue3_base/src/router/index.ts
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import("../components/HelloWorld.vue"),
+    component: () => import("@/components/HelloWorld.vue"),
   },
 ]
 
